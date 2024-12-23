479bb4864e18188a75ce5fb769c6a37858a00825
The Angry Coder <79235452+TheAngryCoder2@users.noreply.github.com>
2024-12-23 16:43:50 +0000

    wip

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,17 @@
-import { appleImg } from '../utils';
+import { appleImg, searchImg, bagImg } from '../utils';
 
 const Navbar = () => {
   return (
     <header>
       <nav>
-        <img src={appleImg} />
+        <img src={appleImg} alt='Apple' width={14}
+        height={18} />
+
+        <div>
+          <img src={searchImg} alt="search" width={18} height={18}/>
+          <img src={bagImg} alt="bag" width={18} height={18}/>
+
+        </div>
       </nav>
     </header>
   )
