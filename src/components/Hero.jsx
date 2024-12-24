13d49a352ad54198f61067516fa336b1cef23acf
The Angry Coder <79235452+TheAngryCoder2@users.noreply.github.com>
2024-12-24 17:53:00 +0000

    wip

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -24,8 +24,8 @@ const Hero = () => {
     }, [])
 
   useGSAP(() => {
-    gsap.to('#hero', { opacity: 1, delay: 1.9})
-    gsap.to('#cta', { opacity: 1, delay: 1.3, y: -50})
+    gsap.to('#hero', { opacity: 1, delay: 2})
+    gsap.to('#cta', { opacity: 1, delay: 2.5, y: -50})
   }, [])
 
   
@@ -35,7 +35,7 @@ const Hero = () => {
   return (
     <section className="w-full nav-height bg-black relative">
       <div className="h-5/6 w-full flex-center flex-col">
-        <p id='hero' className="hero-title">iPhone 15 Pro</p>
+        <p id='hero' className="hero-title">iPhone 25 Pro</p>
         <div className="md:w-10/12 w-9/12">
         <video className='pointer-events-none' autoPlay muted playsInline={true} key={videoSrc}>
           <source src={videoSrc} type='video/mp4' />
@@ -44,7 +44,7 @@ const Hero = () => {
       </div>
       <div id="cta" className='flex flex-col items-center opacity-0 translate-y-20'>
         <a href="#highlights" className='btn'>Buy</a>
-        <p className='font-normal text-xl'> From £139 per month or £1047</p>
+        <p className='font-normal text-xl'> From £139 per month or £10470</p>
       </div>
     </section>
   )
